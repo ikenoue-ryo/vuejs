52b2cc5613bcc327ccd6266fda5f03fee904cf36
github ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-04-16 16:40:54 +0900

    first commit

---
+++ src/views/Projects.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="projects">
+    <h1 class="subheading grey--text">Projects</h1>
+
+    <v-container class="my-5">
+
+      <v-expansion-panel>
+        <v-expansion-panel-content v-for="project in myProjects" :key="project.title">
+          <div slot="header">{{project.title}}</div>
+          <v-card>
+            <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{ project.due }}</div>
+              <div>{{ project.content }}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores neque blanditiis doloremque nisi tempora. Autem sapiente tempora facere veritatis. Sapiente ipsam deserunt eum corrupti, aliquam iure! Voluptate esse facere corrupti." },
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Dec 2018', status: 'complete', content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores neque blanditiis doloremque nisi tempora. Autem sapiente tempora facere veritatis. Sapiente ipsam deserunt eum corrupti, aliquam iure! Voluptate esse facere corrupti." },
+        { title: 'Zizo a new website', person: 'Anny Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores neque blanditiis doloremque nisi tempora. Autem sapiente tempora facere veritatis. Sapiente ipsam deserunt eum corrupti, aliquam iure! Voluptate esse facere corrupti." },
+        { title: 'Baby up the homepage', person: 'Mon Li', due: '10th Dec 2018', status: 'overdue', content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores neque blanditiis doloremque nisi tempora. Autem sapiente tempora facere veritatis. Sapiente ipsam deserunt eum corrupti, aliquam iure! Voluptate esse facere corrupti." },
+        { title: 'Baby up the homepage', person: 'The Net Ninja', due: '9th Dec 2020', status: 'overdue', content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores neque blanditiis doloremque nisi tempora. Autem sapiente tempora facere veritatis. Sapiente ipsam deserunt eum corrupti, aliquam iure! Voluptate esse facere corrupti." },
+      ]
+    }
+  },
+  computed: {
+    myProjects(){
+      return this.projects.filter(project =>{
+        return project.person === 'The Net Ninja'
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="team">
+    <h1 class="subheading grey--text">Team</h1>
+
+    <v-container class="my-5">
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
+          <v-card flat class="text-xs-center ma-3">
+            <v-responsive class="pt-4">
+              <v-avatar size="100">
+                <img :src="person.avatar">
+              </v-avatar>
+            </v-responsive>
+            <v-card-text>
+              <div class="subheading">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="grey">
+                <v-icon small left>message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      team: [
+        { name: 'The Net Ninja', role: 'Web developer', avatar: '/1.jpg'},
+        { name: 'Ryu', role: 'Graphic designer', avatar: '/2.jpg' },
+        { name: 'Chun Li', role: 'Web developer', avatar: '/3.jpg' },
+        { name: 'Gouken', role: 'Social media maverick', avatar: '/4.jpg' },
+        { name: 'Yoshi', role: 'Sales guru', avatar: '/5.jpg' },
+        { name: 'Lizap', role: 'I am Lizap', avatar: '/6.jpg' },
+      ]
+    }
+  }
+}
+</script>
